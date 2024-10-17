fe7eec956e7f2e39406973b8a355049d42f66877
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2024-10-17 13:25:04 -0300

    feat: 🎸 create page html structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,97 @@
 export default function Home() {
-  return <div>Landin page</div>;
+  return (
+    <main>
+      {/* Primeira Seção */}
+      <section>
+        <nav>
+          <img src="" alt="" />
+
+          <div>
+            <button>Funcionamento</button>
+            <button>Preço</button>
+            <button>Login</button>
+          </div>
+        </nav>
+
+        <h1>Simplifique Seus Estudos</h1>
+        <p>
+          Deixe que nós fazermos a curadoria para você. Assine nossa plataforma
+          e receba todos os meses um ebook novo de programação.
+        </p>
+
+        <form>
+          <input type="text" placeholder="Coloque seu email" />
+          <button>Assine Agora</button>
+        </form>
+        <p>Comece sua assinatura agora mesmo. Cancele quando quiser. </p>
+      </section>
+
+      {/* Segunda Seção */}
+      <section>
+        <h2>Como funciona?</h2>
+
+        <div>
+          <img src="" alt="" />
+          <ul>
+            <li>Acesso a 1 ebook por mês</li>
+            <li>Curadoria especial</li>
+            <li>Cancele quando quiser</li>
+          </ul>
+        </div>
+      </section>
+
+      {/* Terceira Seção */}
+      <section>
+        <h2>Preço Simples e Transparente</h2>
+        <p>
+          Pra que inúmeros planos quando nós sabemos exatamente o que é melhor
+          para você? Assine o nosso plano mensal Pro Premium VIP e garanta
+          mensalmente um ebook novo de programação. E por menos de um café por
+          dia.
+        </p>
+
+        <article>
+          <div>
+            <h3>Plano Pro Premium VIP</h3>
+            <p>Tudo que você precisa para seus estudos</p>
+          </div>
+
+          <section>
+            <p>R$29/mês</p>
+
+            <ul>
+              <li>1 ebbok por mês</li>
+              <li>Curadoria especial</li>
+              <li>Acesso ilimitado</li>
+              <li>Cancele a qualquer momento</li>
+            </ul>
+          </section>
+
+          <button>Assine Agora</button>
+        </article>
+      </section>
+
+      {/* Quarta Seção */}
+      <section>
+        <h2>Pronto Para Mudar Sua Vida?</h2>
+        <p>
+          Faça como milhares de outras pessoas. Assine nosso produto e tenha
+          garantido seus estudos{' '}
+        </p>
+
+        <button>Assine Agora</button>
+
+        <p>Comece sua assinatura agora mesmo. Cancele quando quiser. </p>
+      </section>
+
+      {/* Rodapé */}
+      <footer>
+        <div>
+          <img src="" alt="" />
+          <p>LivroSaaS</p>
+        </div>
+        <p>© 2024 LivroSaaS. Todos os direitos reservados.</p>
+      </footer>
+    </main>
+  );
 }
