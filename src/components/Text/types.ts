a697b2e92e09f8ab4c58bf8f1b157a276ee70ff1
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2024-10-18 12:18:58 -0300

    refactor: 💡 Text props adjustments

--- src/components/Text/types.ts
+++ src/components/Text/types.ts
@@ -2,4 +2,8 @@ import { VariantProps } from 'class-variance-authority';
 import { textVariants } from './styles';
 
 export type TextProps = VariantProps<typeof textVariants> &
-  React.HTMLAttributes<HTMLHeadElement>;
+  (
+    | React.HTMLAttributes<HTMLHeadElement>
+    | React.HTMLAttributes<HTMLParagraphElement>
+    | React.HTMLAttributes<HTMLSpanElement>
+  );
